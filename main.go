614c27ce269f8d23c6ea0983464df84f67e5d849
DraconDev <dracsharp@gmail.com>
2023-12-27 12:12:35 +0000

    done

--- main.go
+++ main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to my website!")
-	})
+
+	// * Routes
+	func() {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Welcome to my website!")
+		})
+
+		// Route for "/about" page
+		http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "About the website")
+		})
+
+		// Route for "/contact" page
+		http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Contact us at: contact@example.com")
+		})
+	}()
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// Define the port
+	port := ":8080" // for example, on localhost:8080
+	fmt.Printf("Server starting on port %s\n", port)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	
+
+	// Start the server
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
